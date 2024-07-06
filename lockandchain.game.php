acec34cb87e09721505acc9c23a5065b4ecd12c4
David Renne <davidrenne@gmail.com>
2024-07-06 02:38:13 -0400

    this array works to get into a game

--- lockandchain.game.php
+++ lockandchain.game.php
@@ -27,52 +27,54 @@ class LockAndChain extends Table
         self::DbQuery("START TRANSACTION");
 
         // SQL queries for creating necessary tables without foreign key constraints
-        $sql = "
-            CREATE TABLE IF NOT EXISTS `Cards` (
-                `id` INT AUTO_INCREMENT PRIMARY KEY,
-                `number` INT NOT NULL,
-                `color` VARCHAR(7) NOT NULL
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        $sqlStatements = [
+          "CREATE TABLE IF NOT EXISTS `Cards` (
+                        `id` INT AUTO_INCREMENT PRIMARY KEY,
+                        `number` INT NOT NULL,
+                        `color` VARCHAR(7) NOT NULL
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;",
 
-            CREATE TABLE IF NOT EXISTS `PlayerHands` (
-                `id` INT AUTO_INCREMENT PRIMARY KEY,
-                `player_id` INT,
-                `card_id` INT
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+          "CREATE TABLE IF NOT EXISTS `PlayerHands` (
+                        `id` INT AUTO_INCREMENT PRIMARY KEY,
+                        `player_id` INT,
+                        `card_id` INT
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;",
 
-            CREATE TABLE IF NOT EXISTS `CardPlacements` (
-                `id` INT AUTO_INCREMENT PRIMARY KEY,
-                `game_id` INT,
-                `card_id` INT,
-                `player_id` INT,
-                `position` INT
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+          "CREATE TABLE IF NOT EXISTS `CardPlacements` (
+                        `id` INT AUTO_INCREMENT PRIMARY KEY,
+                        `game_id` INT,
+                        `card_id` INT,
+                        `player_id` INT,
+                        `position` INT
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;",
 
-            CREATE TABLE IF NOT EXISTS `Chains` (
-                `id` INT AUTO_INCREMENT PRIMARY KEY,
-                `player_id` INT,
-                `start_position` INT,
-                `end_position` INT
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+          "CREATE TABLE IF NOT EXISTS `Chains` (
+                        `id` INT AUTO_INCREMENT PRIMARY KEY,
+                        `player_id` INT,
+                        `start_position` INT,
+                        `end_position` INT
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;",
 
-            CREATE TABLE IF NOT EXISTS `Locks` (
-                `id` INT AUTO_INCREMENT PRIMARY KEY,
-                `player_id` INT,
-                `start_position` INT,
-                `end_position` INT
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+          "CREATE TABLE IF NOT EXISTS `Locks` (
+                        `id` INT AUTO_INCREMENT PRIMARY KEY,
+                        `player_id` INT,
+                        `start_position` INT,
+                        `end_position` INT
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;",
 
-            CREATE TABLE IF NOT EXISTS `GameActions` (
-                `id` INT AUTO_INCREMENT PRIMARY KEY,
-                `game_id` INT,
-                `player_id` INT,
-                `action_type` VARCHAR(50),
-                `card_id` INT,
-                `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-            ";
+          "CREATE TABLE IF NOT EXISTS `GameActions` (
+                        `id` INT AUTO_INCREMENT PRIMARY KEY,
+                        `game_id` INT,
+                        `player_id` INT,
+                        `action_type` VARCHAR(50),
+                        `card_id` INT,
+                        `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+        ];
 
-        self::DbQuery($sql);
+        foreach ($sqlStatements as $sql) {
+          self::DbQuery($sql);
+        }
 
         // Initialize players
         $sql = "INSERT INTO player (player_id, player_name, player_color, player_canal, player_avatar) VALUES ";
@@ -102,7 +104,6 @@ class LockAndChain extends Table
       }
     }
   }
-
 
   private function initDecks()
   {
