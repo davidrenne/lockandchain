1ede49ea409d0352e3448b0f1a7b63526b2b7655
David Renne <davidrenne@gmail.com>
2024-07-06 02:36:44 -0400

    debugging fatals

--- lockandchain.view.php
+++ lockandchain.view.php
@@ -1,44 +1,31 @@
 <?php
+
+require_once (APP_BASE_PATH . "view/common/game.view.php");
+
 class view_lockandchain_lockandchain extends game_view
 {
   function getGameName()
   {
     return "lockandchain";
   }
-
   function build_page($viewArgs)
   {
+    // Custom view logic here
+    global $g_user;
     $players = $this->game->loadPlayersBasicInfos();
     $template = self::getGameName() . "_" . self::getGameName();
-    $this->page->begin_block($template, "player_hand");
-    $this->page->begin_block($template, "player_card");
-    $this->page->begin_block($template, "grid_row");
-    $this->page->begin_block($template, "grid_cell");
 
-    // Player hands
-    foreach ($players as $player_id => $player) {
-      $this->page->insert_block(
-        "player_hand",
+    // Sample code to place player panels
+    foreach ($players as $player_id => $info) {
+      $this->tpl['PLAYER_PANEL'] .= self::_(
+        "{$template}_player_panel.tpl",
         array(
-          "PLAYER_ID" => $player_id,
-          "PLAYER_NAME" => $player['player_name']
+          'PLAYER_ID' => $player_id,
+          'PLAYER_NAME' => $info['player_name']
         )
       );
     }
-
-    // Board
-    for ($row = 1; $row <= 6; $row++) {
-      for ($col = 1; $col <= 6; $col++) {
-        $cellId = ($row - 1) * 6 + $col;
-        $this->page->insert_block(
-          "grid_cell",
-          array(
-            "ROW" => $row,
-            "COL" => $col,
-            "CELL_ID" => $cellId
-          )
-        );
-      }
-    }
   }
 }
+
+?>
