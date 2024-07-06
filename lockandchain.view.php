d51ca3bda6b4786ecd05e7d8e00be875781f5819
David Renne <davidrenne@gmail.com>
2024-07-06 03:09:49 -0400

    debug

--- lockandchain.view.php
+++ lockandchain.view.php
@@ -16,23 +16,42 @@ class view_lockandchain_lockandchain extends game_view
     $players = $this->game->loadPlayersBasicInfos();
     $template = self::getGameName() . "_" . self::getGameName();
 
+    // Ensure PLAYER_PANEL is initialized
+    if (!isset($this->tpl['PLAYER_PANEL'])) {
+      $this->tpl['PLAYER_PANEL'] = '';
+    }
+
     // Begin block for player cards in hand
     $this->page->begin_block($template, "player_card");
 
     // Sample code to place player cards
     foreach ($players as $player_id => $info) {
-      // Assuming you have a method to get player cards
       $cards = $this->game->getPlayerCards($player_id);
       foreach ($cards as $card) {
-        $this->page->insert_block("player_card", array(
-          'CARD_ID' => $card['id'],
-          'CARD_COLOR' => $card['color'],
-          'CARD_NUMBER' => $card['number']
-        )
+        $this->page->insert_block(
+          "player_card",
+          array(
+            'CARD_ID' => $card['id'],
+            'CARD_COLOR' => $card['color'],
+            'CARD_NUMBER' => $card['number']
+          )
         );
       }
     }
 
-    // No need to add player board logic here as the grid is static in the template
+    // Ensure PLAYER_BOARD is initialized
+    if (!isset($this->tpl['PLAYER_BOARD'])) {
+      $this->tpl['PLAYER_BOARD'] = '';
+    }
+
+    // Adding the player boards to the main container
+    foreach ($players as $player_id => $info) {
+      $this->tpl['PLAYER_BOARD'] .= '<div id="player_board_' . $player_id . '" class="player_board"></div>';
+    }
+
+    // Insert player boards
+    $this->page->insert_block("player_board", array());
   }
 }
+
+?>
